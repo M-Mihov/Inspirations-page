2002c4b6cfb22b6847e594884c69dbcebf19d3db
m-mihov <m.mihov.wd@outlook.com>
2022-06-12 10:39:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Responsive product inspirational page with expanding content.
---
+++ mood.js
@@ -0,0 +1,65 @@
+$(".mood1").click(function () {
+  $("#mood1-expand").toggle("slow");
+});
+
+$(".mood2").click(function () {
+  $("#mood2-expand").toggle("slow");
+});
+
+$(".mood3").click(function () {
+  $("#mood3-expand").toggle("slow");
+});
+
+$(".mood4").click(function () {
+  $("#mood4-expand").toggle("slow");
+});
+
+$(".mood5").click(function () {
+  $("#mood5-expand").toggle("slow");
+});
+
+$(".mood6").click(function () {
+  $("#mood6-expand").toggle("slow");
+});
+
+$(".mood7").click(function () {
+  $("#mood7-expand").toggle("slow");
+});
+
+$(".mood8").click(function () {
+  $("#mood8-expand").toggle("slow");
+});
+
+$(".mood9").click(function () {
+  $("#mood9-expand").toggle("slow");
+});
+
+$(".mood10").click(function () {
+  $("#mood10-expand").toggle("slow");
+});
+
+$(".mood11").click(function () {
+  $("#mood11-expand").toggle("slow");
+});
+
+$(".mood12").click(function () {
+  $("#mood12-expand").toggle("slow");
+});
+
+$(".mood13").click(function () {
+  $("#mood13-expand").toggle("slow");
+});
+
+$(".mood14").click(function () {
+  $("#mood14-expand").toggle("slow");
+});
+
+$(".mood15").click(function () {
+  $("#mood15-expand").toggle("slow");
+});
+
+$(function () {
+  $(".choices-item img").click(function () {
+    $(this).toggleClass("fade-click");
+  });
+});
